651ebd82a55a607f5deffddda309e84df7b320be
Jacob Ankney <Jacob@Jacobs-MacBook-Pro.local>
2019-04-29 16:29:17 -0600

    working on JS

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log('whatup son ')
+
+let header= document.getElementsByTagName('header')[0]
+
+function addNavBack() {
+ console.log('fired')
+ if(!header.classList.contains('navBack')) {
+  header.classList.add('navBack')
+  console.log(header)
+ }
+}
+
+
+//scroll event listener
+
+header.addEventListener("click", addNavBack)
